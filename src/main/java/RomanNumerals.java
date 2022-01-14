ad13b4b92ef1ba7c9488c07372b0437880f86be0
punkason <punkason@gmail.com>
2022-01-14 22:22:23 +0000

    refactor - merge convertX() to convert()

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,7 +1,6 @@
 public class RomanNumerals {
     private int number;
     private String output;
-    private int j;
     private static final int[] VALUE = {1,5,10,50,100,500,1000};
     private static final String[] ROMAN = {"I","V","X","L","C","D","M"};
 
@@ -27,73 +26,45 @@ public class RomanNumerals {
     }
 
     public void convert(){
+        int i;
         do{
             if(number >= VALUE[6]){
-                j = 6;
-                output += convertM(number);
+                i = 6;
+                output += convert(i);
             }
             else if(number >= VALUE[5]){
-                j = 5;
-                output += convertD(number);
+                i = 5;
+                output += convert(i);
             }
             else if(number >= VALUE[4]){
-                j = 4;
-                output += convertC(number);
+                i = 4;
+                output += convert(i);
             }
             else if(number >= VALUE[3]){
-                j = 3;
-                output += convertL(number);
+                i = 3;
+                output += convert(i);
             }
             else if(number >= VALUE[2]){
-                j = 2;
-                output += convertX(number);
+                i = 2;
+                output += convert(i);
             }
             else if(number >= VALUE[1]){
-                j = 1;
-                output += convertV(number);
+                i = 1;
+                output += convert(i);
             }
             else if(number >= VALUE[0]){
-                j = 0;
-                boolean nearDigit = nearNextDigit(j);
+                i = 0;
+                boolean nearDigit = nearNextDigit(i);
                 if(!nearDigit){
-                    output += convertI(number);
+                    output += convert(i);
                 }
             }
         }while(number > 0);
     }
 
-    private String convertI(int i){//1
-        number = number - VALUE[0];
-        return ROMAN[0];
+    private String convert(int i){
+        number = number - VALUE[i];
+        return ROMAN[i];
     }
 
-    private String convertV(int i){//5
-        number = number - VALUE[1];
-        return ROMAN[1];
-    }
-
-    private String convertX(int i){//10
-        number = number - VALUE[2];
-        return ROMAN[2];
-    }
-
-    private String convertL(int i){//50
-        number = number - VALUE[3];
-        return ROMAN[3];
-    }
-
-    private String convertC(int i){//100
-        number = number - VALUE[4];
-        return ROMAN[4];
-    }
-
-    private String convertD(int i){//500
-        number = number - VALUE[5];
-        return ROMAN[5];
-    }
-
-    private String convertM(int i){//1000
-        number = number - VALUE[6];
-        return ROMAN[6];
-    }
 }
