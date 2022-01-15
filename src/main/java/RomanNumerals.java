6ee8fbd0c0cc3d89fdfc3d0ca068048a07e10b03
punkason <punkason@gmail.com>
2022-01-15 00:28:17 +0000

    merge substractivePrinciple() into convert()

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -8,32 +8,27 @@ public class RomanNumerals {
         output = "";
     }
 
-    // "I" can be subtracted only from "V" and "X"
-    // "X" can be subtracted only from "L" and "C"
-    // "C" can be subtracted only from "D" and "M"
-    private boolean subtractivePrinciple(int i){
-
-        // if i = "V", j = 1 and checking with "I"
-        // if i = "X", j = 2 and checking with "I", but not "V"
-        // 9 should be "IX", not "VIV"
-        int j = 2 - i % 2;
-        if (number >= (VALUE[i] - VALUE[i-j])){ //Can subtract from the next two higher "digits"
-            number += VALUE[i-j]; //adjust the number to next numerals
-            output += ROMAN[i-j]; //output the numerals in front, e.g. 4 will be IV
-            return true;
-        }
-        return false;
-    }
-
     public void convert(){
         int i = ROMAN.length - 1;
         do{
-            if (number >= VALUE[i]) {
+            boolean action = false;
+            if (number >= VALUE[i]) {//Normal Conversion
                 number = number - VALUE[i];
                 output += ROMAN[i];
-            }else if (!subtractivePrinciple(i)){
-                i--;
+                action = true;
+            }else{ //Subtractive Principle
+                // if i = "V", j = 1 and checking with "I"
+                // if i = "X", j = 2 and checking with "I", but not "V"
+                // 9 should be "IX", not "VIV"
+                int j = 2 - i % 2;
+                if (number >= (VALUE[i] - VALUE[i-j])){ //Can subtract from the next two higher "digits"
+                    number += VALUE[i-j]; //adjust the number to next numerals
+                    output += ROMAN[i-j]; //output the numerals in front, e.g. 4 will be IV
+                    action = true;
+                }
             }
+            if (!action) //shift one numeral if no action is taken
+                i--;
         }while(number > 0);
     }
 
