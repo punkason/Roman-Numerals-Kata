d113bbfb2d06e8bd76d278348137e029c5f2d3ef
punkason <punkason@gmail.com>
2022-01-15 00:08:54 +0000

    merge convertToRN into convert

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -8,63 +8,40 @@ public class RomanNumerals {
         output = "";
     }
 
-    public void setNumber(int i){
-        number = i;
-    }
+    // "I" can be subtracted only from "V" and "X"
+    // "X" can be subtracted only from "L" and "C"
+    // "C" can be subtracted only from "D" and "M"
+    private boolean subtractivePrinciple(int i){
 
-    public String getNumber(){
-        return output;
-    }
-
-    private boolean nearNextDigit(int i){
-        if (number >= (VALUE[i+1] - VALUE[i])){
-            number += VALUE[i];
-            output += ROMAN[i];
+        // if i = "V", j = 1 and checking with "I"
+        // if i = "X", j = 2 and checking with "I", but not "V"
+        // 9 should be "IX", not "VIV"
+        int j = 2 - i % 2;
+        if (number >= (VALUE[i] - VALUE[i-j])){ //Can subtract from the next two higher "digits"
+            number += VALUE[i-j]; //adjust the number to next numerals
+            output += ROMAN[i-j]; //output the numerals in front, e.g. 4 will be IV
             return true;
         }
         return false;
     }
 
     public void convert(){
-        int i;
+        int i = ROMAN.length - 1;
         do{
-            if(number >= VALUE[6]){
-                i = 6;
-                output += convert(i);
-            }
-            else if(number >= VALUE[5]){
-                i = 5;
-                output += convert(i);
-            }
-            else if(number >= VALUE[4]){
-                i = 4;
-                output += convert(i);
-            }
-            else if(number >= VALUE[3]){
-                i = 3;
-                output += convert(i);
-            }
-            else if(number >= VALUE[2]){
-                i = 2;
-                output += convert(i);
-            }
-            else if(number >= VALUE[1]){
-                i = 1;
-                output += convert(i);
-            }
-            else if(number >= VALUE[0]){
-                i = 0;
-                boolean nearDigit = nearNextDigit(i);
-                if(!nearDigit){
-                    output += convert(i);
-                }
+            if (number >= VALUE[i]) {
+                number = number - VALUE[i];
+                output += ROMAN[i];
+            }else if (!subtractivePrinciple(i)){
+                i--;
             }
         }while(number > 0);
     }
 
-    private String convert(int i){
-        number = number - VALUE[i];
-        return ROMAN[i];
+    public void setNumber(int i){
+        number = i;
     }
 
+    public String getOutput(){
+        return output;
+    }
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -16,198 +16,210 @@ public class RomanNumeralsTest {
     public void check1(){
         rn.setNumber(1);
         rn.convert();
-        assertEquals("I", rn.getNumber());
+        assertEquals("I", rn.getOutput());
     }
 
     @Test
     public void check5(){
         rn.setNumber(5);
         rn.convert();
-        assertEquals("V", rn.getNumber());
+        assertEquals("V", rn.getOutput());
     }
 
     @Test
     public void check10(){
         rn.setNumber(10);
         rn.convert();
-        assertEquals("X", rn.getNumber());
+        assertEquals("X", rn.getOutput());
     }
 
     @Test
     public void check50(){
         rn.setNumber(50);
         rn.convert();
-        assertEquals("L", rn.getNumber());
+        assertEquals("L", rn.getOutput());
     }
 
     @Test
     public void check100(){
         rn.setNumber(100);
         rn.convert();
-        assertEquals("C", rn.getNumber());
+        assertEquals("C", rn.getOutput());
     }
 
     @Test
     public void check500(){
         rn.setNumber(500);
         rn.convert();
-        assertEquals("D", rn.getNumber());
+        assertEquals("D", rn.getOutput());
     }
 
     @Test
     public void check1000(){
         rn.setNumber(1000);
         rn.convert();
-        assertEquals("M", rn.getNumber());
+        assertEquals("M", rn.getOutput());
     }
 
     @Test
     public void check2(){
         rn.setNumber(2);
         rn.convert();
-        assertEquals("II", rn.getNumber());
+        assertEquals("II", rn.getOutput());
     }
 
     @Test
     public void check3(){
         rn.setNumber(3);
         rn.convert();
-        assertEquals("III", rn.getNumber());
+        assertEquals("III", rn.getOutput());
     }
 
     @Test
     public void check6(){
         rn.setNumber(6);
         rn.convert();
-        assertEquals("VI", rn.getNumber());
+        assertEquals("VI", rn.getOutput());
     }
 
     @Test
     public void check7(){
         rn.setNumber(7);
         rn.convert();
-        assertEquals("VII", rn.getNumber());
+        assertEquals("VII", rn.getOutput());
     }
 
     @Test
     public void check8(){
         rn.setNumber(8);
         rn.convert();
-        assertEquals("VIII", rn.getNumber());
+        assertEquals("VIII", rn.getOutput());
     }
 
     @Test
     public void check11(){
         rn.setNumber(11);
         rn.convert();
-        assertEquals("XI", rn.getNumber());
+        assertEquals("XI", rn.getOutput());
     }
 
     @Test
     public void check13(){
         rn.setNumber(13);
         rn.convert();
-        assertEquals("XIII", rn.getNumber());
+        assertEquals("XIII", rn.getOutput());
     }
 
     @Test
     public void check18(){
         rn.setNumber(18);
         rn.convert();
-        assertEquals("XVIII", rn.getNumber());
+        assertEquals("XVIII", rn.getOutput());
     }
 
     @Test
     public void check51(){
         rn.setNumber(51);
         rn.convert();
-        assertEquals("LI", rn.getNumber());
+        assertEquals("LI", rn.getOutput());
     }
 
     @Test
     public void check55(){
         rn.setNumber(55);
         rn.convert();
-        assertEquals("LV", rn.getNumber());
+        assertEquals("LV", rn.getOutput());
     }
 
     @Test
     public void check60(){
         rn.setNumber(60);
         rn.convert();
-        assertEquals("LX", rn.getNumber());
+        assertEquals("LX", rn.getOutput());
     }
 
     @Test
     public void check101(){
         rn.setNumber(101);
         rn.convert();
-        assertEquals("CI", rn.getNumber());
+        assertEquals("CI", rn.getOutput());
     }
 
     @Test
     public void check300(){
         rn.setNumber(300);
         rn.convert();
-        assertEquals("CCC", rn.getNumber());
+        assertEquals("CCC", rn.getOutput());
     }
 
     @Test
     public void check600(){
         rn.setNumber(600);
         rn.convert();
-        assertEquals("DC", rn.getNumber());
+        assertEquals("DC", rn.getOutput());
     }
 
     @Test
     public void check1001(){
         rn.setNumber(1001);
         rn.convert();
-        assertEquals("MI", rn.getNumber());
+        assertEquals("MI", rn.getOutput());
     }
 
     @Test
     public void check2000(){
         rn.setNumber(2000);
         rn.convert();
-        assertEquals("MM", rn.getNumber());
+        assertEquals("MM", rn.getOutput());
     }
 
     @Test
     public void check4(){
         rn.setNumber(4);
         rn.convert();
-        assertEquals("IV", rn.getNumber());
+        assertEquals("IV", rn.getOutput());
     }
 
     @Test
     public void check9(){
         rn.setNumber(9);
         rn.convert();
-        assertEquals("IX", rn.getNumber());
+        assertEquals("IX", rn.getOutput());
     }
 
+    @Test
     public void check40(){
         rn.setNumber(40);
         rn.convert();
-        assertEquals("XL", rn.getNumber());
+        assertEquals("XL", rn.getOutput());
     }
 
+    @Test
     public void check90(){
         rn.setNumber(90);
         rn.convert();
-        assertEquals("XC", rn.getNumber());
+        assertEquals("XC", rn.getOutput());
     }
 
+    @Test
     public void check400(){
         rn.setNumber(400);
         rn.convert();
-        assertEquals("CD", rn.getNumber());
+        assertEquals("CD", rn.getOutput());
     }
 
+    @Test
     public void check900(){
         rn.setNumber(900);
         rn.convert();
-        assertEquals("CM", rn.getNumber());
+        assertEquals("CM", rn.getOutput());
+    }
+
+    //There is no need to be able to convert numbers larger than about 3000
+    @Test
+    public void check2999(){
+        rn.setNumber(2999);
+        rn.convert();
+        assertEquals("MMCMXCIX", rn.getOutput());
     }
 }
